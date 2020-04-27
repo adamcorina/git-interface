82d49ec900a8808cb319ae31a7503cd53745922b
Corina Adam <adam97corina@gmail.com>
2020-04-27 13:57:52 +0300

    open directory from browser

---
+++ electron-starter.js
@@ -0,0 +1,57 @@
+const electron = require("electron");
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const dialog = electron.dialog;
+const ipcMain = electron.ipcMain;
+
+const path = require("path");
+const url = require("url");
+
+let mainWindow;
+
+function createWindow() {
+  mainWindow = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: { nodeIntegration: true },
+  });
+  const startUrl =
+    process.env.ELECTRON_START_URL ||
+    url.format({
+      pathname: path.join(__dirname, "/../build/index.html"),
+      protocol: "file:",
+      slashes: true,
+    });
+  mainWindow.loadURL(startUrl);
+
+  mainWindow.on("closed", function () {
+    mainWindow = null;
+  });
+}
+
+app.on("ready", createWindow);
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") {
+    app.quit();
+  }
+});
+
+app.on("activate", function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
+
+ipcMain.on("selectDirectory", function () {
+  dialog
+    .showOpenDialog(mainWindow, {
+      properties: ["openDirectory"],
+    })
+    .then((directory) => {
+      if(directory && directory){
+          
+          console.log(directory.filePaths);
+      }
+    });
+});
--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+const electron = window.require("electron");
+const ipcRenderer = electron.ipcRenderer;
 
 function App() {
+  const [searchPath] = useState("");
+
+  const openDirectory = () => {
+    ipcRenderer.send("selectDirectory");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" value={searchPath} readOnly />
+      <button onClick={openDirectory}>Browse</button>
     </div>
   );
 }
