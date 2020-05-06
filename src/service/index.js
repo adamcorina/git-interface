6c0a23c4708dcd3c3d4c696da52e7a7629dbb175
Corina Adam <adam97corina@gmail.com>
2020-05-06 10:43:46 +0300

    do not send request if already in progress

--- src/service/index.js
+++ src/service/index.js
@@ -12,13 +12,32 @@ ipcRenderer.on("asynchronous-reply", (event, arg) => {
   }
 });
 
+function createPromise(_resolve, _reject) {
+  const promise = new Promise(function (resolve, reject) {});
+
+  promise.resolve = _resolve;
+  promise.reject = _reject;
+
+  return promise;
+}
+
 export default function callService(messageType, data) {
+  let duplicateRequestUUID = null;
+  Object.entries(callCache).forEach((cacheEntry) => {
+    if (cacheEntry[1].messageType === messageType && JSON.stringify(cacheEntry[1].data) === JSON.stringify(data)) {
+      duplicateRequestUUID = cacheEntry[0];
+    }
+  });
+  if (duplicateRequestUUID) {
+    return createPromise(callCache[duplicateRequestUUID].resolve, callCache[duplicateRequestUUID].reject);
+  }
   const uuid = uuid4(); //generate the unique call id
   const promise = new Promise((resolve, reject) => {
     callCache[uuid] = {
       resolve,
       reject,
       data,
+      messageType
     };
   });
   ipcRenderer.send("asynchronous-message", { uuid, messageType, data });
